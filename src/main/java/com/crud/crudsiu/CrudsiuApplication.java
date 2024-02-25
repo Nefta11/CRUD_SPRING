bb97e7e168667c1756ea277e8f6e5ce2282505e0
Nefta11 <neftaliarturohernandez@gmail.com>
2024-02-24 20:21:38 -0600

    Updated

---
+++ src/main/java/com/crud/crudsiu/CrudsiuApplication.java
@@ -0,0 +1,13 @@
+package com.crud.crudsiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudsiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudsiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/crudsiu/CrudsiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudsiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
